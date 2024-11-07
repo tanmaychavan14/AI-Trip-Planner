ed6b828548aaecb6afedeb0a2f494195675c1170
Valeria D <vdolgaliova@gmail.com>
2024-11-06 22:22:40 -0500

    Update .gitignore, add Google Places autocomplete, and improve Header and Hero components

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Button} from '../ui/button'
+import { Button } from '../ui/button'
+import {Link} from 'react-router-dom'
 
 function Hero() {
   return (
@@ -8,12 +9,12 @@ function Hero() {
         className="font-extrabold text-[50px] font-family-[Inter] text-[#6b493c] text-center mt-20"
         style={{ fontFamily: 'Inter, sans-serif' }}
       >
-        <span className="text-[#864228]">Explore Smarter & Plan Less with AI-Curated Itineraries:</span><br/><span className="text-[30px]">Custom travel plans that bring your adventures to life, stress-free and with ease</span>
+        <span className="text-[#864228]">Explore Smarter & Plan Less with AI-Curated Itineraries:</span><br /><span className="text-[30px]">Custom travel plans that bring your adventures to life, stress-free and with ease</span>
       </h1>
-        <p className='text-xl text-brown-300 text-center'>Your personalized travel planner, crafting custom itineraries that perfectly match your interests and budget.</p>
+      <p className='text-xl text-brown-300 text-center'>Your personalized travel planner, crafting custom itineraries that perfectly match your interests and budget.</p>
 
       <Link to={'/create-trip'}>
-      <Button className='bg-[#6b493c] text-white rounded  hover:bg-[#805545] hover:text-white hover:border-[#805545]'>Begin Your Adventure Now!</Button>
+        <Button className='bg-[#6b493c] text-white rounded  hover:bg-[#805545] hover:text-white hover:border-[#805545]'>Begin Your Adventure Now!</Button>
       </Link>
     </div>
   )
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
+  const [place, setPlace] = useState();
   return (
-    <div>CreateTrip</div>
+    <div className="flex justify-center">
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+      <h2 className='font-bold text-blue-400 text-3xl'>Share Your Travel Preferences 🗺️🏝️</h2>
+      <p className='mt-3 text-amber-800 text-xl'>Give a few details about what you like, and we’ll create a personalized itinerary that fits your travel style.</p>
+    
+    <div className='mt-20'>
+      <div>
+       <h2 className='text-xl my-3 font-medium'>What is your preferred destination?</h2>
+       <GooglePlacesAutocomplete
+         apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+         selectProps={{
+          place,
+          onChange: (v) =>{setPlace(v); console.log(v)}
+         }}
+       />
+      </div>
+    </div>
+    
+    </div>
+    </div>
   )
 }
 
