0b9eeca3528b0faa7a2c36f5c78a4f36b234794a
Valeria D <vdolgaliova@gmail.com>
2025-01-06 03:17:41 -0500

    Added location, duration, budget, and number of travelers

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -1,11 +1,21 @@
 
 
-function InfoSection() {
+
+
+function InfoSection({trip}) {
   return (
     <div>
         <img src='/assets/placeholder.png' className='h-[350px] w-full object-cover rounded-xl'/>
     
-    
+    <div className="my-5 flex flex-col gap-2">
+      <h2 className="font-bold text-2xl">{trip?.userSelection?.location?.label}</h2>
+    </div>
+        <div className="flex gap-5"> 
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-grey-600">📅 {trip.userSelection?.noOfDays} Day(s)</h2>
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-grey-600">💲{trip.userSelection?.budget} Budget</h2>
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-grey-600">👨‍👩‍👧‍👦 Number of travelers: {trip.userSelection?.people}</h2>
+
+        </div>
     </div>
   )
 }
