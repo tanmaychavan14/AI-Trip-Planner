2a09d2b676dad580c25f4f4c183074ab21548add
Valeria D <vdolgaliova@gmail.com>
2025-01-07 04:32:49 -0500

    Added hotel price and rating

--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -9,15 +9,19 @@ function Hotels({trip}) {
          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5"> 
 
            {trip?.tripData?.hotels?.map((hotel, index) => (
-                
-                <div>   
+
+                // eslint-disable-next-line react/jsx-key
+                <div className="hover:scale-105 transition-all cursor-pointer">   
                     <img src="/assets/placeholder.png"  className="rounded-xl"/>
                     
-                    <div className="my-3"> 
+                    <div className="my-3 flex flex-col gap-2"> 
  
-                      <h2 className="font-medium">{hotel.hotelName}</h2>
-                      <h2 className="text-xs text-gray-700">📍{hotel.hotelAddress}</h2>
-                       
+                      <h2 className="font-medium">{hotel?.hotelName}</h2>
+                      <h2 className="text-xs text-gray-700">📍{hotel?.hotelAddress}</h2>
+                      <h2 className="text-sm">💰{hotel?.price}</h2>
+                      <h2 className="text-sm">⭐{hotel?.rating}</h2>
+
+
                     </div>
                  </div>
 
