31df770e699095552f1dbbfaf3dfdacd4fb2392c
Valeria D <vdolgaliova@gmail.com>
2025-01-11 20:54:33 -0500

    Removed sign in button if user signed in

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -3,18 +3,23 @@ import { useEffect } from 'react';
 
 function Header() {
 
-const user=JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem('user'));
 
-useEffect(()=>{
-  console.log(user);
-})
+  useEffect(() => {
+    console.log(user);
+  })
 
   return (
     <div className='p-2 shadow-md flex justify-between items-center px-5'>
       <img src='/assets/logo.svg' width={150} />
 
       <div>
-        <Button className='bg-[#6b493c] text-white rounded  hover:bg-[#805545] hover:text-white hover:border-[#805545]'>Sign In</Button>
+        {user ? <div> </div> :
+
+          <Button className='bg-[#6b493c] text-white rounded  hover:bg-[#805545] hover:text-white hover:border-[#805545]'>Sign In</Button>
+
+        }
+
       </div>
     </div>
   )
