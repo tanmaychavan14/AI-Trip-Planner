8fc0a3eed2867d1e67f8a9ad46670d6c3057c4aa
Valeria D <vdolgaliova@gmail.com>
2025-01-11 21:06:02 -0500

    Added popover component

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,5 +1,10 @@
 import { Button } from '@/components/ui/button';
 import { useEffect } from 'react';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 function Header() {
 
@@ -14,7 +19,12 @@ function Header() {
       <img src='/assets/logo.svg' width={150} />
 
       <div>
-        {user ? <div> </div> :
+        {user ?
+
+          <div className='flex items-center gap-5'>
+            <Button variant="outline" className="bg-[#6b493c] text-white rounded  hover:bg-[#805545] hover:text-white hover:border-[#805545] rounded-full">My Trips</Button>
+            <img src={user?.picture} className=" h-[45px] w-[45px] rounded-full"/>
+          </div> :
 
           <Button className='bg-[#6b493c] text-white rounded  hover:bg-[#805545] hover:text-white hover:border-[#805545]'>Sign In</Button>
 
